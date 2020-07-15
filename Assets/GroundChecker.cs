e2d062e8d6737d0b62494857ca077d8ab4fc7bfe
nikitakartsevv <67075231+nikitakartsevv@users.noreply.github.com>
2020-07-15 11:30:41 -0700

    fps person

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public GameObject player;
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().isGrounded = true;
+            print("testing");  
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Ground")
+        {
+            player.GetComponent<PlayerMovement>().isGrounded = false;
+
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,53 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController controller;
+    //public CharacterController controller;
 
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
-    public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
+ 
     Vector3 velocity;
-    bool isGrounded;
+    public bool isGrounded;
 
 
     void Update()
     {
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
         print(isGrounded);
+        /*
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-
+        */
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        transform.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+        //Vector3 move = transform.right * x + transform.forward * z;
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
+        //controller.Move(move * speed * Time.deltaTime);
 
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             print("jump");
         }
 
-        velocity.y += gravity * Time.deltaTime;
+        //velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity * Time.deltaTime);
+        //controller.Move(velocity * Time.deltaTime);
 
 
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            print("testing");
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+
+        }
     }
 }
