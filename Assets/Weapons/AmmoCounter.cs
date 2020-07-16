e3987f5717d4c963f2116cc49cb3f25b9734d9cb
Simon Zhao <important.mail.simon@gmail.com>
2020-07-16 14:53:02 -0700

    Weapons
    - 3 weapons
    - weapon switching
    - shooting
    - reload system
    - Basic ammo UI
    - reload animation
    - knife attack animation
    - can't reload at 0 ammo
    - can't shoot at 0 ammo
    - ammo icon that refills ammo for one gun (you can randomly spawn those in if you want)

---
+++ Assets/Weapons/AmmoCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCounter : MonoBehaviour
+{
+    //public GameObject AmmoTextMesh; 
+    public string currentAmmoOnGun;
+    public string ammoOwnForGun;
+
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+        if (FindObjectOfType<Weapon_Switching>().selectedWeapon == 0)
+        {
+            currentAmmoOnGun = FindObjectOfType<Gun_AK47>().currentAmmo.ToString();
+            ammoOwnForGun = FindObjectOfType<Gun_AK47>().ammoOwn.ToString();
+            GetComponent<TMPro.TextMeshProUGUI>().text = currentAmmoOnGun + " / " + ammoOwnForGun;
+        }
+        else if (FindObjectOfType<Weapon_Switching>().selectedWeapon == 1)
+        {
+            currentAmmoOnGun = FindObjectOfType<Gun_Pistol>().currentAmmo.ToString();
+            ammoOwnForGun = FindObjectOfType<Gun_Pistol>().ammoOwn.ToString();
+            GetComponent<TMPro.TextMeshProUGUI>().text = currentAmmoOnGun + " / " + ammoOwnForGun;
+        }
+        else if (FindObjectOfType<Weapon_Switching>().selectedWeapon == 2)
+        {
+            GetComponent<TMPro.TextMeshProUGUI>().text = "Infinite";
+        }
+     
+
+   }
+}
