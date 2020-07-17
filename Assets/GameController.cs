87d723a154b398a9c9b7a5b4485fdb0d89b62cac
nikitakartsevv <67075231+nikitakartsevv@users.noreply.github.com>
2020-07-17 11:38:27 -0700

    zombie spawner

---
+++ Assets/GameController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public int waveNumber;
+    public GameObject zombieCounter;
+
+    private bool spawning = true;
+
+    void Start()
+    {
+        waveNumber = 1;
+        StartCoroutine(SpawnWaves());
+    }
+    void Update ()
+    {
+        if(!spawning && zombieCounter.transform.childCount == 0)
+        {
+            StartCoroutine(SpawnWaves());
+            spawning = true;
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        
+        spawning = true;
+        for (int i = 0; i < hazardCount + (waveNumber * 5); i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            GameObject copy = Instantiate(hazard, spawnPosition, spawnRotation);
+            copy.transform.parent = zombieCounter.transform;
+            yield return new WaitForSeconds(spawnWait);
+        }
+        spawning = false;
+        waveNumber++;
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+    static int playerScore = 0;
+
+    public GUISkin mySkin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public static void AddPoints(int pointValue)
+    {
+        playerScore += pointValue;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        GUIStyle style1 = mySkin.customStyles[0];
+
+        GUI.Label(new Rect(40, Screen.height - 80, 100, 60), "Score :");
+        GUI.Label(new Rect(100, Screen.height - 80, 100, 60), "" + playerScore, style1);
+    }
+}
