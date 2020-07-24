723e7636c5ebe67c9465d978e34a0d3b622f4400
nikitakartsevv <67075231+nikitakartsevv@users.noreply.github.com>
2020-07-24 12:57:54 -0700

    changes

---
+++ Assets/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public string name;
+    public PlayerHealth script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = FindObjectOfType<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(script.currentHealth <= 0)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+        }
+    }
+}
